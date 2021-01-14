596f7cc86d5e4f7f56eb92cfcdd367b45b725b69
yamadadada <297354270@qq.com>
2021-01-14 23:45:49 +0800

    用户权限认证

---
+++ common/src/main/java/com/yamada/auth/TokenRelayRequestInterceptor.java
@@ -0,0 +1,28 @@
+package com.yamada.auth;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenRelayRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        // 1. 获取到Token
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes attributes = (ServletRequestAttributes)requestAttributes;
+        HttpServletRequest request = attributes.getRequest();
+        String token = request.getHeader("X-Token");
+
+        // 2. 将token传递
+        if (!StringUtils.isEmpty(token)) {
+            requestTemplate.header("X-Token", token);
+        }
+    }
+
+}
