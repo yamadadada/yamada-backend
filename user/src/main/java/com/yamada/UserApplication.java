a780cc8d321dd5b0e8798d15e15a4f6c73977401
yamadadada <297354270@qq.com>
2021-01-25 00:52:48 +0800

    fix bug

--- user/src/main/java/com/yamada/UserApplication.java
+++ user/src/main/java/com/yamada/UserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @MapperScan("com.yamada.mapper")
 public class UserApplication {
 
--- user/src/main/java/com/yamada/entity/User.java
+++ user/src/main/java/com/yamada/entity/User.java
@@ -2,6 +2,7 @@ package com.yamada.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.Date;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @TableId(type = IdType.AUTO)
