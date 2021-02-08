b1cc9717a6135f77906ddaee554cd7f36c9b07d9
yamadadada <297354270@qq.com>
2021-02-08 16:40:50 +0800

    2.8

--- user/src/main/java/com/yamada/user/UserApplication.java
+++ user/src/main/java/com/yamada/user/UserApplication.java
@@ -1,8 +1,13 @@
 package com.yamada.user;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
@@ -11,5 +16,16 @@ public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+
+        try {
+            String serverAddr = "111.230.220.64:8848";
+            Properties properties = new Properties();
+            properties.put("serverAddr", serverAddr);
+            properties.put("username", "yamada");
+            properties.put("password", "");
+            ConfigService configService = NacosFactory.createConfigService(properties);
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
     }
 }
